a1ef4867fe8b8aa329c189dfc3933e57f07b0eee
MahmodMG <121952530+MahmodMG@users.noreply.github.com>
2023-07-12 11:57:39 +0300

    first commit

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,1 +1,2 @@
-module.exports.baseUrl = "https://mg-ecommerce-2qq0.onrender.com";
+// module.exports.baseUrl = "https://mg-ecommerce-2qq0.onrender.com";
+module.exports.baseUrl = "https://ecommerce-back-pdhs.onrender.com";
